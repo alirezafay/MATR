466adf03c905971f803a68eeb6ce7b3b923224b9
alirezafay <149387054+alirezafay@users.noreply.github.com>
2024-04-20 23:31:50 +0330

    Update Test.py

--- Test.py
+++ Test.py
@@ -40,9 +40,9 @@ def fusion():
     for num in range(1):
         tic = time.time()
 
-        path1 = '/kaggle/working/MATR/1111_fake_B.png'
+        path1 = '/kaggle/working/MATR/020.gif'
 
-        path2 = '/kaggle/working/MATR/1111.png'
+        path2 = '/kaggle/working/MATR/020_2.gif'
 
         img1 = Image.open(path1).convert('L')
         img2 = Image.open(path2).convert('L')
